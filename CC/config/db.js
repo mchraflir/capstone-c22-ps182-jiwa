682c80a69618050c362ffdebf9940db44b8af989
azzahrachalista <68979750+azzahrachalista@users.noreply.github.com>
2022-06-13 11:55:30 +0700

    Update db.js

--- CC/config/db.js
+++ CC/config/db.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const sequelize = require("sequelize");
 
-const db = new sequelize("mymentaldb", "root", "", {
-    dialect:"mysql"
+const db = new sequelize(process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+    host: process.env.MYSQLHOST,
 });
 
 db.sync({});
